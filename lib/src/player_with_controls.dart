7c7699ef0b59345716b81f4eb6ccc6a515c033ec
Ahmadre <rebar.ahmad@telexiom.de>
2020-03-22 23:31:21 +0100

    Corrected AspectRatio to support Portrait

--- lib/src/player_with_controls.dart
+++ lib/src/player_with_controls.dart
@@ -1,7 +1,7 @@
+import 'dart:math';
 import 'dart:ui';
 
 import 'package:chewie/src/chewie_player.dart';
-import 'package:chewie/src/cupertino_controls.dart';
 import 'package:chewie/src/material_controls.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
@@ -14,59 +14,62 @@ class PlayerWithControls extends StatelessWidget {
   Widget build(BuildContext context) {
     final ChewieController chewieController = ChewieController.of(context);
 
-    return Center(
-      child: Container(
-        width: MediaQuery.of(context).size.width,
-        child: AspectRatio(
-          aspectRatio:
-              chewieController.aspectRatio ?? _calculateAspectRatio(context),
-          child: _buildPlayerWithControls(chewieController, context),
-        ),
-      ),
-    );
-  }
+    double _calculateAspectRatio(BuildContext context) {
+      final size = MediaQuery.of(context).size;
+      final width = size.width;
+      final height = size.height;
 
-  Container _buildPlayerWithControls(
-      ChewieController chewieController, BuildContext context) {
-    return Container(
-      child: Stack(
-        children: <Widget>[
-          chewieController.placeholder ?? Container(),
-          Center(
-            child: AspectRatio(
-              aspectRatio: chewieController.aspectRatio ??
-                  _calculateAspectRatio(context),
-              child: VideoPlayer(chewieController.videoPlayerController),
+      return width > height ? width / height : height / width;
+    }
+
+    double _calculateScale() {
+      final double videoAspectRatio =
+          chewieController.videoPlayerController.value.aspectRatio;
+      final Size screenSize = MediaQuery.of(context).size;
+      final double screenAspectRatio = screenSize.aspectRatio;
+      return max(screenAspectRatio / videoAspectRatio,
+          videoAspectRatio / screenAspectRatio);
+    }
+
+    Widget _buildControls(
+      BuildContext context,
+      ChewieController chewieController,
+    ) {
+      return chewieController.showControls
+          ? chewieController.customControls != null
+              ? chewieController.customControls
+              : MaterialControls()
+          : Container();
+    }
+
+    Container _buildPlayerWithControls(
+        ChewieController chewieController, BuildContext context) {
+      return Container(
+        child: Stack(
+          children: <Widget>[
+            chewieController.placeholder ?? Container(),
+            Center(
+              child: AspectRatio(
+                aspectRatio:
+                    chewieController.videoPlayerController.value.aspectRatio,
+                child: VideoPlayer(chewieController.videoPlayerController),
+              ),
             ),
-          ),
-          chewieController.overlay ?? Container(),
-          _buildControls(context, chewieController),
-        ],
+            chewieController.overlay ?? Container(),
+            _buildControls(context, chewieController),
+          ],
+        ),
+      );
+    }
+
+    return Center(
+        child: Container(
+      height: MediaQuery.of(context).size.height,
+      width: MediaQuery.of(context).size.width,
+      child: AspectRatio(
+        aspectRatio: _calculateAspectRatio(context),
+        child: _buildPlayerWithControls(chewieController, context),
       ),
-    );
-  }
-
-  Widget _buildControls(
-    BuildContext context,
-    ChewieController chewieController,
-  ) {
-    return chewieController.showControls
-        ? chewieController.customControls != null
-            ? chewieController.customControls
-            : Theme.of(context).platform == TargetPlatform.android
-                ? MaterialControls()
-                : CupertinoControls(
-                    backgroundColor: Color.fromRGBO(41, 41, 41, 0.7),
-                    iconColor: Color.fromARGB(255, 200, 200, 200),
-                  )
-        : Container();
-  }
-
-  double _calculateAspectRatio(BuildContext context) {
-    final size = MediaQuery.of(context).size;
-    final width = size.width;
-    final height = size.height;
-
-    return width > height ? width / height : height / width;
+    ));
   }
 }
